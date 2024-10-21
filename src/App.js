b8e511e10ead95f55b65893b4b6c5369b08ec485
Cathy Jin <cathyking716@gmail.com>
2024-10-20 19:05:18 -0700

    Add Planet Focalizer section.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 function App() {
   const [isExpanded, setIsExpanded] = useState(false);
   const [loading, setLoading] = useState(false);  // Add loading state
+  const [isDominantPlanetResultCollapsed, setIsDominantPlanetResultCollapsed] = useState(false);
+  const [isLifeThemeResultCollapsed, setIsLifeThemeResultCollapsed] = useState(false);
 
   const [formData, setFormData] = useState({
     year: '',
@@ -27,6 +29,14 @@ function App() {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
+  const toggleDominantPlanetResultCollapse = () => {
+    setIsDominantPlanetResultCollapsed(!isDominantPlanetResultCollapsed);
+  };
+
+  const toggleLifeThemeResultCollapse = () => {
+    setIsLifeThemeResultCollapsed(!isLifeThemeResultCollapsed);
+  };
+
   // Handler for checkbox change (DST)
   // const handleDSTChange = (e) => {
   //   setFormData({ ...formData, isDst: e.target.checked });
@@ -34,6 +44,8 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setIsDominantPlanetResultCollapsed(false);
+    setIsLifeThemeResultCollapsed(false);
     setLoading(true);  // Start loading
     try {
       const response = await fetch('https://astro-notebook.onrender.com/report', {
@@ -105,9 +117,11 @@ function App() {
       <br />
       <div className="info_collector">
         <form onSubmit={handleSubmit}>
-          <p>
-            <h3><b>输入你的基本信息</b></h3>
+            <div className="info_collector_header">
+              <h3><b>输入你的基本信息</b></h3>
+            </div>
             <div className="birth-time-row">
+              <p>
               <b>出生日期</b>
               <br />
               <select name="year" value={formData.year} onChange={handleChange}>
@@ -158,9 +172,10 @@ function App() {
               分
               {/* <br />  
               <input type="checkbox" checked={formData.isDst} onChange={handleDSTChange} />是否是夏令时 */}
+              </p>
             </div>
-            <br />
             <div className="birth-location-row">
+              <p>
               <b>出生地点</b>
               <br />国家/地区：
               <input type="text" name="country" size="10" placeholder="中国" value={formData.country} onChange={handleChange} />
@@ -168,13 +183,13 @@ function App() {
               <input type="text" name="state" size="10" placeholder="江苏省" value={formData.state} onChange={handleChange} />
               <br />城市：
               <input type="text" name="city" size="10" placeholder="苏州市" value={formData.city} onChange={handleChange} />
+              </p>
             </div>
-          </p>
           <button type="submit">生成结果</button>
           </form>
       </div>
       {loading && (<div><p>正在努力计算，请稍后。。。</p></div>)}
-      {result && (<div className="result">{renderResult(result)}</div>)}
+      {result && (<div className="result">{renderResult(result, isDominantPlanetResultCollapsed, toggleDominantPlanetResultCollapse, isLifeThemeResultCollapsed, toggleLifeThemeResultCollapse)}</div>)}
       <footer className="footer">
         <hr /><a href="mailto:cathyking716@gmail.com">Contact Me</a>
       </footer>
@@ -183,60 +198,85 @@ function App() {
   
 }
 
-function renderResult(result) {
+
+function renderResult(result, isDominantPlanetResultCollapsed, toggleDominantPlanetResultCollapse, isLifeThemeResultCollapsed, toggleLifeThemeResultCollapse) {
+  // Handle errors first
   if (typeof result === 'object' && result !== null && result.hasOwnProperty('error')) {
     if (result.error.includes("Invalid input: ")) {
       return (
         <>
           <div className="error">请确保所有时间都已正确填写。</div>
         </>
-      )
+      );
     } else if (result.error.includes("Unable to find the coordinates.")) {
       return (
         <>
           <div className="error">无法找到地点，请确保地点信息都已正确填写。</div>
         </>
-      )
+      );
     } else if (result.error.includes("Unable to find the timezone")) {
       return (
         <>
           <div className="error">无法找到出生地时区，请刷新或重试。</div>
         </>
-      )
+      );
     } else {
       return (
         <>
           <div className="error">似乎没有找到结果，请刷新或重试。</div>
         </>
-      )
+      );
     }
-  } else if (typeof result === 'object' && result !== null && result.hasOwnProperty('themes')) {
-    result = result.themes
-    if (result.length === 0) {
-      return (
-        <> 
-          <h2>计算结果</h2> 
-          <p>似乎没有找到结果，请刷新或重试。</p>
-        </>
-      )
-    } else {
-      return (
-        <> 
-          <h2>计算结果（能量出现频率从高到低）</h2> 
-          {renderEnergies(result)}
-        </>
-      )
-    }
+  }
+
+  // Handle report results
+  if (typeof result === 'object' && result !== null && result.hasOwnProperty('themes') && result.hasOwnProperty('planet_focalizers')) {
+    return (
+      <>
+        {renderPlanetaryFocalizers(result.planet_focalizers, isDominantPlanetResultCollapsed, toggleDominantPlanetResultCollapse)}
+        <br />
+        {renderThemes(result.themes, isLifeThemeResultCollapsed, toggleLifeThemeResultCollapse)}
+      </>
+    );
   } else {
     return (
       <>
         <div className="error">似乎没有找到结果，请刷新或重试。</div>
       </>
-    )
+    );
   }
 }
 
-function renderEnergies(energies) {
+
+function renderThemes(themes, isLifeThemeResultCollapsed, toggleLifeThemeResultCollapse) {
+  if (themes.length === 0) {
+    return (
+      <> 
+        <div className="report_item">
+          <h2>人生主题</h2> 
+          <p>似乎没有找到结果，请刷新或重试。</p>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <> 
+        <div className="report_item">
+          <h2>人生主题 <button className="collapse-btn" onClick={toggleLifeThemeResultCollapse}>{isLifeThemeResultCollapsed ? '+' : '-'}</button></h2>
+          <div className="report_item_summary">
+            <p>在本命盘上出现三次及以上的占星规则的组合，就是人生主题。人生主题可以帮助认识自我以及指明成长的方向。计算结果按照能量出现的次数<b>从高到低</b>排列。对本命盘的进一步分析应结合太阳、月亮、上升点等其他因素综合判断。</p>
+          </div>
+          <div className={`content ${isLifeThemeResultCollapsed ? 'hidden' : ''}`}>
+            {!isLifeThemeResultCollapsed && renderThemesContent(themes)}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+
+function renderThemesContent(energies) {
   return energies.map((energy, index) => (
       <div key={index}>
         <hr />
@@ -245,12 +285,13 @@ function renderEnergies(energies) {
   ));
 }
 
+
 function renderEnergy(e) {
   return (
       <>
           <h3>主题：{e.name_cn}</h3>
-          <p><b>关键词（仅供参考）</b>：{renderKeywords(e.principle_kws_cn, e.principle_nums)}</p>
-          <p><b>解读（仅供参考）</b>：{e.interpretation_cn}</p>
+          <p><b>关键词（仅供参考，需要结合星盘全局分析）</b>：{renderEnergyKeywords(e.principle_kws_cn, e.principle_nums)}</p>
+          <p><b>解读（仅供参考，需要结合星盘全局分析）</b>：<br />{e.interpretation_cn}</p>
           <p><b>星盘中出现次数</b>：{e.patterns_cn.length}</p>
           <p><b>星盘中表现形式</b>：
               <ul>
@@ -263,13 +304,15 @@ function renderEnergy(e) {
   );
 }
 
-function renderKeywords(kws, p_nums) {
+
+function renderEnergyKeywords(kws, p_nums) {
   if (kws.length === 1) {
     return (
       <>
+        <br />
         {kws[0]}
       </>
-    )
+    );
   } else {
     return (
       <>
@@ -278,9 +321,105 @@ function renderKeywords(kws, p_nums) {
           <li>{p_nums[1]}的能量包含：{kws[1]}</li>
         </ul>
       </>
-    )
+    );
   }
 }
 
+
+function renderPlanetaryFocalizers(focalizers, isDominantPlanetResultCollapsed, toggleDominantPlanetResultCollapse) {
+  if (focalizers.length === 0) {
+    return (
+      <> 
+        <div className="report_item">
+          <h2>重点行星</h2> 
+          <p>似乎没有找到结果，请刷新或重试。</p>
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <> 
+        <div className="report_item">
+          <h2>重点行星 <button className="collapse-btn" onClick={toggleDominantPlanetResultCollapse}>{isDominantPlanetResultCollapsed ? '+' : '-'}</button></h2> 
+          <div className="report_item_summary">
+            <p>重点行星是本命盘中能量强大的行星，对全局有较为突出的影响。对本命盘的进一步分析应结合太阳、月亮、上升点等其他因素综合判断。</p>
+          </div>
+          <div className={`content ${isDominantPlanetResultCollapsed ? 'hidden' : ''}`}>
+            {!isDominantPlanetResultCollapsed && renderFocalizersContent(focalizers)}
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+function renderFocalizersContent(focalizers) {
+  const domScore = focalizers[0].score
+  const domPlanets = focalizers.filter(item => item.score === domScore)
+  return (
+    <>
+        <hr />
+        <h3>重点行星：{concatenatePlanetNames(domPlanets)}</h3>
+        <p>
+          <b>解读（仅供参考，需要结合星盘全局分析）</b>：
+          {domPlanets.length === 1 ? (
+              <>
+                <br />
+                {domPlanets[0].interpretation_cn}
+              </>
+            ) : (
+              <ul>
+                {domPlanets.map(planet => (<li key={planet.id}>{planet.interpretation_cn}</li>))}
+              </ul>
+            )
+          }
+        </p>
+        <hr />
+        <h3>得分详情</h3>
+        <p>
+          <table className="planet_focialier_result_table">
+            <thead>
+              <tr>
+                <th>行星</th>
+                <th>得分</th>
+                <th>星盘中表现形式</th>
+              </tr>
+            </thead>
+            <tbody>
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "太阳")} 
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "月亮")} 
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "水星")} 
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "金星")} 
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "火星")} 
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "木星")} 
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "土星")} 
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "天王星")} 
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "海王星")} 
+              {renderPlanetFocalizerTableRow(focalizers, domScore, "冥王星")} 
+            </tbody>
+        </table>
+        </p>
+    </>
+  );
+}
+
+function concatenatePlanetNames(domPlanets) {
+  if (domPlanets.length === 1) {
+    return domPlanets[0].name_cn;
+  } else {
+    return domPlanets.map(item => item.name_cn).join('、 ');
+  }
+}
+
+function renderPlanetFocalizerTableRow(focalizers, domScore, name_cn) {
+  const focalizer = focalizers.filter(item => item.name_cn === name_cn)[0]
+  return (
+    <tr>
+      <td>{name_cn}</td>
+      <td>{focalizer.score === domScore ? <b>{focalizer.score}</b> : focalizer.score}</td>
+      <td className='focalizer_pattern_row'>{focalizer.patterns_cn.join('；')}</td>
+    </tr>
+  );
+}
 
 export default App;
